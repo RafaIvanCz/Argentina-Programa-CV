c0fea7b06e3f8f860b62791fb69f2900afd154f5
RafaelIvanCz <rafaelivancz@gmail.com>
2023-02-16 16:56:41 -0300

    Se agrega Javascript a btn-img e input email

---
+++ js/app.js
@@ -0,0 +1,16 @@
+document.getElementById('img-button').onclick = function() {
+  document.getElementById('img-button').innerHTML = "Iván Cruz <br/> Desarrollador Front-end";
+  document.getElementById('img-button').style.backgroundColor = '#2b2d42'
+  document.getElementById('img-button').style.border = '2px solid #590d22'
+  document.getElementById('img-button').style.color = 'white'
+}
+
+function validarEmail(email) {
+  const regExEmail =  /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  const esValido = regExEmail.test(email);
+  if (esValido === true) {
+    alert('El email es válido')
+  } else {
+    alert('El email NO es válido')
+  }
+}
