5af455a36cd8d9f95c2db513c2d6f0cea22e2674
RafaelIvanCz <rafaelivancz@gmail.com>
2023-03-02 23:50:19 -0300

    Se creó prompt con saludo de bienvenida.

--- js/app.js
+++ js/app.js
@@ -5,6 +5,12 @@ document.getElementById('img-button').onclick = function() {
   document.getElementById('img-button').style.color = 'white'
 }
 
+const nombreUsuario = prompt('Ingrese su nombre, por favor.')
+document.getElementById('saludo-prompt').innerHTML = `Bienvenido/a ${nombreUsuario} a mi CV❗  😁👍🎉`
+document.getElementById('saludo-prompt').style.backgroundColor = '#e7d7c1'
+document.getElementById('saludo-prompt').style.border = '2px solid #590d22'
+document.getElementById('saludo-prompt').style.color = '#2b2d42'
+
 function validarEmail(email) {
   const regExEmail =  /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
   const emailValido = regExEmail.test(email);
